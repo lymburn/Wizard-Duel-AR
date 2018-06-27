8c8fd70ebf3cc267694e110a743718702d1c685f
Eugene Lu <eugenelu9848@gmail.com>
2018-06-26 19:03:09 -0600

    Implement physics body for projecting spells and support collision detection

--- Wizard Duel AR/ViewController.swift
+++ Wizard Duel AR/ViewController.swift
@@ -10,72 +10,128 @@ import UIKit
 import SceneKit
 import ARKit
 
+enum BitMaskCategory: Int {
+    case spell = 1
+    case target = 2
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
-    
+    var target: SCNNode?
     override func viewDidLoad() {
         super.viewDidLoad()
         setupWand()
         sceneView.delegate = self
+        sceneView.scene.physicsWorld.contactDelegate = self
         sceneView.showsStatistics = true
         sceneView.autoenablesDefaultLighting = true
+        
+        let box = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        box.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        box.position = SCNVector3(0,0,0)
+        box.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(node: box, options: nil))
+        box.physicsBody?.categoryBitMask = BitMaskCategory.target.rawValue
+        box.physicsBody?.contactTestBitMask = BitMaskCategory.spell.rawValue
+        sceneView.scene.rootNode.addChildNode(box)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //Draw channelling spell light
         drawSpellLight()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        sceneView.pointOfView?.enumerateChildNodes {(node, _) in
-            if node.name == "light" {
-                node.removeFromParentNode()
-            }
-        }
+        //Remove the spell light when user stops holding
+        fireSpell()
     }
+    
+    var spell = SCNNode()
+    var wandNode = SCNNode()
     
     fileprivate func setupWand() {
         //Attach wand node to camera view
         let wandScene = SCNScene(named: "art.scnassets/Wand.scn")
-        let wandNode = wandScene?.rootNode.childNode(withName: "Wand", recursively: false)
-        wandNode?.geometry?.firstMaterial?.specular.contents = UIColor.white
-        wandNode?.position = SCNVector3(0.1, -0.2, -0.8)
-        wandNode?.name = "wand"
-        sceneView.pointOfView?.addChildNode(wandNode!)
+        wandNode = (wandScene?.rootNode.childNode(withName: "Wand", recursively: false))!
+        wandNode.geometry?.firstMaterial?.specular.contents = UIColor.white
+        wandNode.position = SCNVector3(0.1, -0.2, -0.8)
+        wandNode.name = "wand"
+        sceneView.pointOfView?.addChildNode(wandNode)
     }
 }
 
 extension ViewController: ARSCNViewDelegate {
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
     }
-    
-    fileprivate func drawSpellLight() {
+}
+
+extension ViewController: SCNPhysicsContactDelegate {
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        let nodeA = contact.nodeA
+        let nodeB = contact.nodeB
+        if nodeA.physicsBody?.categoryBitMask == BitMaskCategory.target.rawValue {
+            //If node A is the target
+            target = nodeA
+            //Remove spell node when hit
+            nodeB.removeFromParentNode()
+        } else {
+            target = nodeB
+            //Remove spell node when hit
+            nodeA.removeFromParentNode()
+        }
+        let explosion = SCNParticleSystem(named: "Explosion.scnp", inDirectory: nil)!
+        explosion.loops = false
+        explosion.particleLifeSpan = 4
+        explosion.particleSize = 0.3
+        let explosionNode = SCNNode()
+        explosionNode.addParticleSystem(explosion)
+        explosionNode.position = contact.contactPoint
+        sceneView.scene.rootNode.addChildNode(explosionNode)
+        target?.removeFromParentNode()
+    }
+}
+
+fileprivate extension ViewController {
+    func drawSpellLight() {
         //Draw a circle indicating a spell is being channelled
-        let ball = SCNNode()
-        ball.position = SCNVector3(0.01, -0.03, -0.8)
-        ball.name = "light"
+        spell = SCNNode()
+        spell.position = SCNVector3(0.01, -0.03, -0.8)
+        spell.name = "spell light"
+        spell.physicsBody?.categoryBitMask = BitMaskCategory.spell.rawValue
+        spell.physicsBody?.contactTestBitMask = BitMaskCategory.target.rawValue
         
         let fire = SCNParticleSystem(named: "Fireball.scnp", inDirectory: nil)!
         fire.particleSize = 0.1
-        ball.addParticleSystem(fire)
-        sceneView.pointOfView?.addChildNode(ball)
+        spell.addParticleSystem(fire)
+        sceneView.pointOfView?.addChildNode(spell)
+    }
+    
+    //Fire a spell
+    func fireSpell() {
+        let orientation = getCameraOrientation()
+        
+        //Apple force to spell
+        let body = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: spell, options: nil))
+        body.isAffectedByGravity = false
+        let force = SCNVector3(orientation.x*5, orientation.y*5, orientation.z*5)
+        body.applyForce(force, asImpulse: true)
+        body.friction = 0.0
+        spell.physicsBody = body
+        
+        //Delete spell after 2 seconds
+        spell.runAction(SCNAction.sequence([SCNAction.wait(duration: 2),
+                                           SCNAction.removeFromParentNode()]))
     }
 }
 
